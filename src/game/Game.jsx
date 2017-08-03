28c3bd838aa67345495c45e0d049af85c2aa50ae
Brian Gruber <bgruber@meetup.com>
2017-08-03 18:07:23 -0400

    calculate rsvp rate and change the tempo

--- src/game/Game.jsx
+++ src/game/Game.jsx
@@ -9,6 +9,7 @@ import RSVPViewer from './RSVPViewer';
 
 const STREAM_URL = 'ws://stream.meetup.com/2/rsvps';
 const RSVP_BUFFER_LENGTH = 2048;
+const RSVP_TIME_SAMPLE_BUFFER_LENGTH = 60;
 function truncateArray(a, max) {
 	if (a.length >= max) {
 		a.pop();
@@ -21,15 +22,37 @@ export default class Game extends Component {
 		super(props);
 		this.state = {
 			rsvps: [],
-			rsvpRate: 80,
-			soundtrack: false
+			rsvpTimer: {
+				times: [],
+				rsvpsPerMin: 80
+			},
+			soundtrack: true
 		};
 		this.handleData = this.handleData.bind(this);
+		this.updateRsvpTimer = this.updateRsvpTimer.bind(this);
+	}
+
+	updateRsvpTimer(timer) {
+		const now = new Date();
+		var rsvpsPerMin = timer.rsvpsPerMin;
+		var times = times = [now, ...timer.times.slice(0, RSVP_TIME_SAMPLE_BUFFER_LENGTH)];
+		if (times.length > 10) {
+			const totalTime = times[0] - times[times.length - 1];
+			const avgPeriodMs = totalTime / (times.length - 1);
+			rsvpsPerMin = 60 * 1000.0 / avgPeriodMs;
+		}
+
+		return {
+			times: times,
+			rsvpsPerMin: rsvpsPerMin
+		};
 	}
 
 	handleData(data) {
 		this.setState((state, props) => ({
-			rsvps: [JSON.parse(data), ...truncateArray(state.rsvps, RSVP_BUFFER_LENGTH)]
+			rsvps: [JSON.parse(data), ...truncateArray(state.rsvps, RSVP_BUFFER_LENGTH)],
+			rsvpTimer: this.updateRsvpTimer(state.rsvpTimer),
+			soundtrack: true
 		}));
 	}
 
@@ -37,7 +60,8 @@ export default class Game extends Component {
     return (
     	<div>
 				<Websocket url={STREAM_URL} onMessage={this.handleData}/>
-				<Soundtrack tempo={this.state.rsvpRate} playing={this.state.soundtrack}/>
+				<Soundtrack tempo={this.state.rsvpTimer.rsvpsPerMin} playing={this.state.soundtrack}/>
+				<div>RSVPs/min: {this.state.rsvpTimer.rsvpsPerMin}</div>
 				<Flex>
 					<FlexItem>
 						<GameBoard rsvsp={this.state.rsvps} />
